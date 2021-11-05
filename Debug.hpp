6689d5c33bb00dacd2cc0ad962e2f88a4a52fd65
Max van Nistelrooij <maxvannistelrooij@gmail.com>
2021-11-05 12:58:16 +0100

    Fixed Debug

--- Debug.hpp
+++ Debug.hpp
@@ -1,7 +1,7 @@
 #ifndef DEBUG_H_
 #define DEBUG_H_
 
-#include "Point.hpp"
+#include "Vector2.hpp"
 #include "Color.hpp"
 #include <string>
 #include "Debug_extra_includes.hpp"
@@ -19,7 +19,7 @@ namespace spic {
          * @param end The end point.
          * @param color The line color, defaults to white.
          */
-        void DrawLine(const Point& start, const Point& end, const Color& color = Color::white());
+        void DrawLine(const Vector2& start, const Vector2& end, const Color& color = Color::white());
 
         /**
          * @brief Logs a message to the Console.
